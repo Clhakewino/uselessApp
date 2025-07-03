38365687be5a456f594b6417f31ad793d88bb2de
Bruyin <chlacchy96@gmail.com>
2025-07-03 17:49:58 +0200

    aggiunto firebase

--- useless_app/lib/main.dart
+++ useless_app/lib/main.dart
@@ -7,7 +7,19 @@ import 'buttonsManager.dart';
 import 'landscape.dart';
 import 'Login.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e, s) {
+    print('Errore inizializzazione Firebase: $e');
+    print(s);
+  }
   runApp(const MyApp());
 }
 
